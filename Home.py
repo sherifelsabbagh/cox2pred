c8bf9bfbc264719fe30a5b1de6a7fb6479e9e947
Sherif Elsabbagh <sherif.elsabbagh@hotmail.com>
2024-11-18 00:01:55 +0200

    s

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.title('COX2 Activity Prediction App')
 st.info('This App can be used to predict whether a molecule would be active or inactive against COX2 target protein.')
 
 st.subheader("App Usage:")
-st.write("The sidebar of the app contains three pages")
+st.write("The sidebar of the app contains three pages:")
 st.write("The first page can be used for prediction of multiple molecules provided in SMILES format (Virtual screening).")
 st.write("The second page contains information regarding the built model performance.")
 st.write("You can find information about COX2 protein in the third page.")
