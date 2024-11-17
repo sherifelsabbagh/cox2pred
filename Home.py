f5b94bc506ae22c29c3d658c84339130d4648c1f
Sherif Elsabbagh <sherif.elsabbagh@hotmail.com>
2024-11-17 23:57:53 +0200

    update home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,11 @@ st.set_page_config(
     page_title="Home")
 
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByQqTdmeNAG0Fhb7TAVN2X8BM9BOX6g0A0g&s")
+
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9i6jUsvad581B5oqj4CQUhVUkeV6zdTaHbw&s")
+
 st.title('COX2 Activity Prediction App')
+
 st.info('This App can be used to predict whether a molecule would be active or inactive against COX2 target protein.')
 
 st.subheader("App Usage:")
@@ -13,7 +17,7 @@ st.write("The first page can be used for prediction of multiple molecules provid
 st.write("The second page contains information regarding the built model performance.")
 st.write("You can find information about COX2 protein in the third page.")
 
-st.footer("© Galala university, 2024")
+st.footer("© Faculty of Pharmacy, Galala university, 2024")
 
 
 
