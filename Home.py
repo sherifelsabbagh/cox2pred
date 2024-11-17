67df19d8549d7b37a1394a5b2a642bf0f2c9b60b
Sherif Elsabbagh <sherif.elsabbagh@hotmail.com>
2024-11-17 23:54:51 +0200

    ssss

--- Home.py
+++ Home.py
@@ -3,14 +3,18 @@ import streamlit as st
 st.set_page_config(
     page_title="Home")
 
-st.title('LRRK2 Activity prediction App')
-st.info('The LRRK2 Activity Prediction App can be used to predict whether a molecule is active or inactive for LRRK2 target protein .')
+st.img("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByQqTdmeNAG0Fhb7TAVN2X8BM9BOX6g0A0g&s")
+st.title('COX2 Activity Prediction App')
+st.info('This App can be used to predict whether a molecule would be active or inactive against COX2 target protein.')
 
-st.subheader("App function:")
-st.write("The sidebar of App Contain four function page")
-st.write("The first page can be used for prediction of only one molecule. It should be provided in SMILES format.")
-st.write("The second page is used for prediction of multiple molecules provided in SMILES format .")
-st.write("The third page has information about model performance.")
-st.write("The last page has information about LRRK2 protein.")
+st.subheader("App Usage:")
+st.write("The sidebar of the app contains three pages")
+st.write("The first page can be used for prediction of multiple molecules provided in SMILES format (Virtual screening).")
+st.write("The second page contains information regarding the built model performance.")
+st.write("You can find information about COX2 protein in the third page.")
 
-st.warning('Limit 250 compounds per file')
+st.footer("© Galala university, 2024")
+
+
+
+
